0a7d32b0b7cb74e5686d1db63cf9875d98d8bcc9
macmini <macmini@macminiui-Mac-mini.local>
2016-08-27 08:22:33 +0900

    init

---
+++ app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { ROUTER_DIRECTIVES } from '@angular/router';
+
+@Component({
+    moduleId: module.id,
+    selector: 'app',
+    templateUrl: 'app.component.html',
+    directives: [ROUTER_DIRECTIVES]
+})
+
+export class AppComponent {
+  public constructor(){
+  
+  }
+}
---
+++ app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { APP_BASE_HREF } from '@angular/common';
+import { AppComponent }  from './app.component';
+import { RouterModule } from '@angular/router';
+import { HttpModule } from '@angular/http';
+import { routing } from './app.routing';
+
+import { HomeComponent } from './home/home';
+
+@NgModule({
+  imports: [ BrowserModule, RouterModule, HttpModule, routing],
+  declarations: [ AppComponent, HomeComponent ],
+  bootstrap: [ AppComponent ]
+})
+
+export class AppModule { }
