a30a669182ca505a5c72f6880ac578aceb4a16a4
gman <im@gman.io>
2016-09-28 23:29:32 +0900

    add

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,14 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { APP_BASE_HREF } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { HttpModule } from '@angular/http';
+import { HttpModule, JsonpModule  } from '@angular/http';
 import { routing } from './app.routing';
 
 import { AppComponent }  from './app.component';
 import { HomeComponent }      from './Home/home';
 
 @NgModule({
-  imports: [ BrowserModule, RouterModule, HttpModule, routing],
+  imports: [ BrowserModule, RouterModule, HttpModule, JsonpModule, routing],
   declarations: [ AppComponent, HomeComponent ],
   bootstrap: [ AppComponent ]
 })
